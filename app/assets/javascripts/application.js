152cec3473bd27477e5da315c4315846d5e8ff67
Adam Kimball <ad00m@Adams-MacBook-Pro.local>
2017-09-05 13:24:47 -0600

    more formatting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require materialize-sprockets
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+  setTimeout(function(){
+    $('#flash').remove();
+  }, 5000);
+});
